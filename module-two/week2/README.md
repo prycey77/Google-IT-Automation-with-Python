58338a4ff3bb03b80244a27e5b5d70032252e9dd
Richard Pryce <rich_pryce@yahoo.co.uk>
2021-03-21 11:22:12 +0000

    Week 2 completed

--- module-two/week2/README.md
+++ module-two/week2/README.md
@@ -80,3 +80,8 @@ Some of the functions available from the os module are:
 
 
 ---
+CSV Files Cheat Sheet
+Check out the following links for more information:
+
+https://docs.python.org/3/library/csv.html
+https://realpython.com/python-csv/
--- module-two/week2/module_2_week_2.py
+++ module-two/week2/module_2_week_2.py
@@ -1,7 +1,7 @@
 # Reading and Writing Files
 # Creates a new file object and assigning it to a variable called file
 # Current line assumes that the spider.txt file is in the same directory
-file = open("using-python-to-interact-with-the-operating-system/week-two/spider.txt")
+file = open("module-two/week2/spider.txt")
 
 # readline method reads a single line of a file
 print(file.readline())
@@ -15,30 +15,30 @@ file.close()
 
 # "with" keyword creates a block of code with the work that needs to be done with the file inside of it
 # When "with" block is used, Python will automatically close the file
-with open("using-python-to-interact-with-the-operating-system/week-two/spider.txt") as file:
+with open("module-two/week2/spider.txt") as file:
     print(file.readline())
 
 # Iterates the file object like list or strings
 # The file has a new line character at the end of each line
 # When Python reads the file line by line, the line variable will always have a new line character at the end
-with open("using-python-to-interact-with-the-operating-system/week-two/spider.txt") as file:
+with open("module-two/week2/spider.txt") as file:
     for line in file:
         print(line.upper())
 
 # Empty lines can be avoided by using strip method
-with open("using-python-to-interact-with-the-operating-system/week-two/spider.txt") as file:
+with open("module-two/week2/spider.txt") as file:
     for line in file:
         print(line.strip().upper())
 
 # Iterates the file object by reading the file lines into a list
-file = open("using-python-to-interact-with-the-operating-system/week-two/spider.txt")
+file = open("module-two/week2/spider.txt")
 lines = file.readlines()
 file.close
 lines.sort()
 print(lines)
 
 # "with" block pattern takes the second argument which specifies in which mode the file should be opened as
-with open("using-python-to-interact-with-the-operating-system/week-two/novel.txt", "w") as file:
+with open("module-two/week2/novel.txt", "w") as file:
     file.write("It was a dark and stormy night")
 
 # Managing Files and Directories
@@ -46,25 +46,25 @@ with open("using-python-to-interact-with-the-operating-system/week-two/novel.txt
 import os
 
 # remove function deletes a file
-os.remove("using-python-to-interact-with-the-operating-system/week-two/novel.txt")
+os.remove("module-two/week2/novel.txt")
 
 # rename function renames a file
 # The first parameter to rename function is the old name of the file and the second is new name
-os.rename("using-python-to-interact-with-the-operating-system/week-two/spider.txt", "using-python-to-interact-with-the-operating-system/week-two/spider_rename.txt")
-os.rename("using-python-to-interact-with-the-operating-system/week-two/spider_rename.txt", "using-python-to-interact-with-the-operating-system/week-two/spider.txt")
+os.rename("module-two/week2/spider.txt", "module-two/week-two/spider_rename.txt")
+os.rename("module-two/week2/spider_rename.txt", "module-two/week-two/spider.txt")
 
 # the OS path sub-module's exists function checks whether a file exist
-print(os.path.exists("using-python-to-interact-with-the-operating-system/week-two/spider_rename.txt"))
+print(os.path.exists("module-two/week2/spider_rename.txt"))
 
 # getsize checks a file size and returns the file size in bytes
-print(os.path.getsize("using-python-to-interact-with-the-operating-system/week-two/spider.txt"))
+print(os.path.getsize("module-two/week2/spider.txt"))
 
 # getmitime checks when the file was last modified and returns Unix timestamp
-print(os.path.getmtime("using-python-to-interact-with-the-operating-system/week-two/spider.txt"))
+print(os.path.getmtime("module-two/week2/spider.txt"))
 
 # datetime module provides a function to convert the Unix timestamp
 import datetime
-timestap = os.path.getmtime("using-python-to-interact-with-the-operating-system/week-two/spider.txt")
+timestap = os.path.getmtime("module-two/week2/spider.txt")
 print(datetime.datetime.fromtimestamp(timestap))
 
 # abspath function takes a filename and turns it into an absolute path
@@ -75,10 +75,10 @@ print(os.path.abspath("spider.txt"))
 print(os.getcwd())
 
 # mkdir function creates a directory
-os.mkdir("using-python-to-interact-with-the-operating-system/week-two/new_dir")
+os.mkdir("module-two/week2/new_dir")
 
 # chdir function changes a directory
-os.chdir("using-python-to-interact-with-the-operating-system/week-two/new_dir")
+os.chdir("module-two/week2/new_dir")
 print(os.getcwd())
 os.chdir("..")
 print(os.getcwd())
@@ -89,9 +89,9 @@ os.rmdir(os.path.abspath("new_dir"))
 os.chdir("..")
 print(os.getcwd())
 # os.listdir function returns a list of all the files and sub-directories in a given directory
-print(os.listdir("week-two"))
-dir = "week-two"
-os.mkdir("week-two/another_new_dir")
+print(os.listdir("week2"))
+dir = "week2"
+os.mkdir("week2/another_new_dir")
 for name in os.listdir(dir):
     # os.path.join creates the full path and it can be used to to check if the files are directory or not
     fullname = os.path.join(dir, name)
@@ -100,11 +100,11 @@ for name in os.listdir(dir):
     else:
         print("{} is a file".format(fullname))
 
-os.rmdir("week-two/another_new_dir")
+os.rmdir("week2/another_new_dir")
 
 # Reading CSV Files
 import csv
-f = open("week-two/csv_file.txt")
+f = open("week2/csv_file.txt")
 
 # parses the file using the CSV module
 csv_f = csv.reader(f)
@@ -114,9 +114,17 @@ for row in csv_f:
     print("Name: {}, Phone: {}, Role: {}".format(name, phone, role))
 f.close()
 
+# Write to csv file
+hosts = [["workstation.local", "192.168.25.46"], ["webserver.cloud", "10.2.5.6"]]
+with open('hosts.csv', 'w') as hosts_csv:
+    writer = csv.writer(hosts_csv)
+    writer.writerows(hosts)
+
+
+
 # Reading and Writing CSV Files with Dictionaries
 # DictReader turns each row of the data in a CSV file into a dictionary
-with open ("week-two/software.csv") as software:
+with open ("week2/software.csv") as software:
     reader = csv.DictReader(software)
     for row in reader:
         print("{} has {} users".format(row["name"], row["users"]))
@@ -128,9 +136,9 @@ users = [{"name": "Sol Mansi", "username": "solm", "department": "IT infrastruct
 
 keys = ["name", "username", "department"]
 
-with open("week-two/by_department.csv", "w") as by_department:
+with open("by_department.csv", "w") as by_department:
     writer = csv.DictWriter(by_department, fieldnames=keys)
     writer.writeheader()
     writer.writerows(users)
 
-os.remove("week-two/by_department.csv")
+os.remove("week2/by_department.csv")
